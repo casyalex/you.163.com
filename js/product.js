33e337fc615f5379242d9791cca26641c1c3b93f
alex <superalex_1104@163.com>
2018-04-21 08:10:54 +0800

    单品图片信息注入

--- js/product.js
+++ js/product.js
@@ -13,9 +13,39 @@ if(!pageId || !curData){
 }
 
 //面包屑的功能
-var positionFn=yx.g('#position .center');
-positionFn.innerHTML='<a href="#">首页</a> > ';
-for (var i=0;i<curData.categoryList.length;i++) {
-	positionFn.innerHTML+='<a href="#"> '+curData.categoryList[i].name+'</a> >';
-}
-positionFn.innerHTML+=' '+curData.name;
+;(function(){
+	var positionFn=yx.g('#position .center');
+	positionFn.innerHTML='<a href="#">首页</a> > ';
+	for (var i=0;i<curData.categoryList.length;i++) {
+		positionFn.innerHTML+='<a href="#"> '+curData.categoryList[i].name+'</a> >';
+	}
+	positionFn.innerHTML+=' '+curData.name;
+})();
+
+//产品图功能
+(function(){
+	var bigImg=yx.g('#productImg .left>img');
+	var smallImg=yx.ga('#productImg .smallImg img');
+	
+	bigImg.src=smallImg[0].src=curData.primaryPicUrl;
+	
+	for (var i=0;i<smallImg.length;i++) {
+		if(i){
+			smallImg[i].src=curData.itemDetail['picUrl'+i];
+		}
+		smallImg[i].onmouseover=function(){
+			for (var i=0;i<smallImg.length;i++){
+				smallImg[i].className='';
+			}
+			bigImg.src=this.src;
+			this.className='active';
+		}
+	}
+	
+	//产品信息导入
+	yx.g('#productImg .info h2').innerHTML=curData.name;
+	yx.g('#productImg .info p').innerHTML=curData.simpleDesc;
+	yx.g('#productImg .info .price').innerHTML='<div><span>售价</span><strong>￥'+curData.retailPrice+'.00'+'</strong></div>'+
+					'<div><span>促销</span><a href="'+curData.hdrkDetailVOList[0].huodongUrlPc+'" class="tag">'+curData.hdrkDetailVOList[0].activityType+'</a><a href="'+curData.hdrkDetailVOList[0].huodongUrlPc+'" class="discount">'+curData.hdrkDetailVOList[0].name+'</a></div>'+
+					'<div><span>服务</span><a href="#" class="service"><i></i>30天无忧退货<i></i>48小时快速退款<i></i>满88元免邮费<i></i>网易自营品牌</a></div>';
+})();
